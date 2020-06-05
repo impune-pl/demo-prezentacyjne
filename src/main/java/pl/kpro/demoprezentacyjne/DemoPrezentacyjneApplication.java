2dcc44893211a922b29fff59a08978d62ecbb61b
Krzysztof Prorok <Krzysztof1397@gmail.com>
2020-06-05 14:52:50 +0200

    Initial Commit

---
+++ src/main/java/pl/kpro/demoprezentacyjne/DemoPrezentacyjneApplication.java
@@ -0,0 +1,15 @@
+package pl.kpro.demoprezentacyjne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPrezentacyjneApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DemoPrezentacyjneApplication.class, args);
+    }
+
+}
